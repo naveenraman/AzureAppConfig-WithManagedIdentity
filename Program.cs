877339b0248eb81fa5d62e74be771cdbf4efb9c0
Atmakur, Naveen <Naveen.Atmakur@wolterskluwer.com>
2020-07-10 18:25:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Azure.Identity;
+
+namespace TestAppConfig
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var settings = config.Build();
+                if (hostingContext.HostingEnvironment.IsDevelopment())
+                {
+                    config.AddAzureAppConfiguration(settings["ConnectionStrings:AppConfig"]);
+                }
+                else
+                {
+                    config.AddAzureAppConfiguration(options =>
+                    options.Connect(new Uri(settings["AppConfig:Endpoint"]), new ManagedIdentityCredential()));
+                }
+            })
+            .UseStartup<Startup>());
+    }
+}
